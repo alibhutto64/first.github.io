27e3113b7f96f5401c8cdda874a2bc485e113e19
Jack Sparrow <randomstuffemail47@gmail.com>
2020-04-08 07:28:37 +0800

    second

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const Base64 = require('js-base64').Base64
 
 const encoded = Base64.encode(`
-ssr://MTM4LjY4LjQ5LjM6ODA4MDphdXRoX3NoYTFfdjQ6Y2hhY2hhMjA6dGxzMS4yX3RpY2tldF9mYXN0YXV0aDpjR0ZyYVhOMFlXNHhNZy8_b2Jmc3BhcmFtPSZncm91cD1kWE5o
-ssr://NDcuMjQwLjI1Mi4zMjo4MDphdXRoX3NoYTFfdjQ6Y2hhY2hhMjA6cGxhaW46Y0dGcmFYTjBZVzR4TWcvP29iZnNwYXJhbT0mZ3JvdXA9Ylhr
+    {
+        "remarks" : "",
+        "id" : "826963D2EB1B0093FB9E49048A92A5F5",
+        "server" : "138.68.49.3",
+        "server_port" : 8080,
+        "server_udp_port" : 0,
+        "password" : "pakistan12",
+        "method" : "chacha20",
+        "protocol" : "auth_sha1_v4",
+        "protocolparam" : "",
+        "obfs" : "tls1.2_ticket_fastauth",
+        "obfsparam" : "",
+        "remarks_base64" : "",
+        "group" : "usa",
+        "enable" : true,
+        "udp_over_tcp" : false
+    },
+    {
+        "remarks" : "",
+        "id" : "0CC4DB1D4CD102549080FC2BBAD343EF",
+        "server" : "47.240.252.32",
+        "server_port" : 80,
+        "server_udp_port" : 0,
+        "password" : "pakistan12",
+        "method" : "chacha20",
+        "protocol" : "auth_sha1_v4",
+        "protocolparam" : "",
+        "obfs" : "plain",
+        "obfsparam" : "",
+        "remarks_base64" : "",
+        "group" : "usa",
+        "enable" : true,
+        "udp_over_tcp" : false
+    }
 `)
 
 console.log(encoded)
+
